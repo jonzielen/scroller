ad56da894ccf86a1336c1ecffe36dd01a16aa47d
jonzielen <jzielenkievicz@nycgo.com>
2017-03-21 11:46:33 -0400

    update plugin

--- js/main.js
+++ js/main.js
@@ -1,30 +1,72 @@
-(function() {
-  var scroller = document.querySelector('.scroller'),
-      scrollerParent = scroller.parentElement,
-      scrollWrapper = document.createElement('div'),
-      head = document.head,
-      styleSheeet = document.createElement('style'),
-      scrollOffset = scrollerParent.getBoundingClientRect().right;
+var NewsScroller = function(obj) {
+  // default settings
+  var settings = {
+    scrollerItem: obj,
+    speed: 1,
+    delay: 0,
+    iteration: 'infinite',
+    swing: 'linear'
+  };
 
-  // css
-  var css = [
-    '.scroll-wrapper {position: relative;}',
-    '.scroller {opacity: 0; position: absolute; white-space: nowrap; animation: 57.52s linear 1s infinite scrollWrapperSlide; margin-left: '+scrollOffset+'px;}',
-    '.scroll-wrapper .scroller {opacity: 1;}'
-  ].join(' ');
+  console.log(obj);
 
-  styleSheeet.innerHTML = css;
-  head.appendChild(styleSheeet);
+  // update default settings
+  function updateSettings(newSettings) {
+    for (var newSetting in newSettings) {
+      if (newSettings.hasOwnProperty(newSetting)) {
+        settings[newSetting] = newSettings[newSetting];
+      }
+    }
 
-  scrollWrapper.classList.add('scroll-wrapper');
-  scrollWrapper.appendChild(scroller);
-  scrollerParent.appendChild(scrollWrapper);
+    init(settings);
+  }
 
-  var scrollerWidth = scrollOffset - scroller.getBoundingClientRect().right;
+  function init(settings) {
+    var scroller = settings.scrollerItem,
+        scrollerParent = scroller.parentElement,
+        scrollWrapper = document.createElement('div'),
+        speed = settings.speed,
+        delay = settings.delay,
+        iteration = settings.iteration,
+        swing = settings.swing,
+        head = document.head,
+        styleSheeet = document.createElement('style'),
+        scrollOffset = scrollerParent.getBoundingClientRect().right;
 
-  var keyframes = [
-    '@keyframes scrollWrapperSlide {from {margin-left: '+scrollOffset+'px;} to {margin-left: '+scrollerWidth+'px;}} '
-  ].join('');
+    // css
+    var css = [
+      '.scroll-wrapper {position: relative;}',
+      '.scroller {opacity: 0; position: absolute; white-space: nowrap;}',
+      '.scroll-wrapper .scroller {opacity: 1;}'
+    ].join('');
 
-  styleSheeet.innerHTML = keyframes + styleSheeet.innerHTML;
-})();
+    scroller.classList.add('scroller');
+    styleSheeet.innerHTML = css;
+    head.appendChild(styleSheeet);
+
+    scrollWrapper.classList.add('scroll-wrapper');
+    scrollWrapper.appendChild(scroller);
+    scrollerParent.appendChild(scrollWrapper);
+
+    var scrollerWidth = scrollOffset - scroller.getBoundingClientRect().right;
+    var animationTime = Math.abs(scrollerWidth / (100 * speed));
+
+    var keyframes = [
+      '@keyframes tickerWrapperSlide {from {margin-left: '+scrollOffset+'px;} to {margin-left: '+scrollerWidth+'px;}}'
+    ];
+
+    var scrollerAnimation = [
+      '.scroller {animation: '+animationTime+'s '+swing+' '+delay+'s '+iteration+' tickerWrapperSlide; margin-left: '+scrollOffset+'px;}'
+    ];
+
+    styleSheeet.innerHTML = [
+      keyframes,
+      scrollerAnimation,
+      styleSheeet.innerHTML
+    ].join('');
+  }
+
+  return {
+    ticker: updateSettings
+  };
+};
