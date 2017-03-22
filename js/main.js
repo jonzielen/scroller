574d2e056cc541035d62bafe637a3944d44c3eab
jonzielen <jzielenkievicz@nycgo.com>
2017-03-22 15:12:46 -0400

    refactor

--- js/main.js
+++ js/main.js
@@ -8,8 +8,6 @@ var NewsScroller = function(obj) {
     swing: 'linear'
   };
 
-  console.log(obj);
-
   // update default settings
   function updateSettings(newSettings) {
     for (var newSetting in newSettings) {
@@ -31,33 +29,29 @@ var NewsScroller = function(obj) {
         swing = settings.swing,
         head = document.head,
         styleSheeet = document.createElement('style'),
-        scrollOffset = scrollerParent.getBoundingClientRect().right;
-
-    // css
-    var css = [
-      '.scroll-wrapper {position: relative;}',
-      '.scroller {opacity: 0; position: absolute; white-space: nowrap;}',
-      '.scroll-wrapper .scroller {opacity: 1;}'
-    ].join('');
+        scrollOffset = scrollerParent.getBoundingClientRect().right,
+        css = [
+          '.scroll-wrapper {position: relative;}',
+          '.scroller {opacity: 0; position: absolute; white-space: nowrap;}',
+          '.scroll-wrapper .scroller {opacity: 1;}'
+        ].join('');
 
     scroller.classList.add('scroller');
-    styleSheeet.innerHTML = css;
-    head.appendChild(styleSheeet);
-
     scrollWrapper.classList.add('scroll-wrapper');
     scrollWrapper.appendChild(scroller);
     scrollerParent.appendChild(scrollWrapper);
 
-    var scrollerWidth = scrollOffset - scroller.getBoundingClientRect().right;
-    var animationTime = Math.abs(scrollerWidth / (100 * speed));
+    styleSheeet.innerHTML = css;
+    head.appendChild(styleSheeet);
 
-    var keyframes = [
-      '@keyframes tickerWrapperSlide {from {margin-left: '+scrollOffset+'px;} to {margin-left: '+scrollerWidth+'px;}}'
-    ];
-
-    var scrollerAnimation = [
-      '.scroller {animation: '+animationTime+'s '+swing+' '+delay+'s '+iteration+' tickerWrapperSlide; margin-left: '+scrollOffset+'px;}'
-    ];
+    var scrollerWidth = -Math.abs(scroller.getBoundingClientRect().right),
+        animationTime = Math.abs(scrollerWidth / (100 * speed)),
+        keyframes = [
+          '@keyframes tickerWrapperSlide {from {margin-left: '+scrollOffset+'px;} to {margin-left: '+scrollerWidth+'px;}}'
+        ],
+        scrollerAnimation = [
+          '.scroller {animation: '+animationTime+'s '+swing+' '+delay+'s '+iteration+' tickerWrapperSlide; margin-left: '+scrollOffset+'px;}'
+        ];
 
     styleSheeet.innerHTML = [
       keyframes,
